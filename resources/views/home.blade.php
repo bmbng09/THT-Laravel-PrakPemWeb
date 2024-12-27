85fb3561572208f25b1a208a6a622a0b8e6efcfd
BambangIstijab <bambangistijab30@gmail.com>
2024-12-27 20:20:16 +0700

    push lagi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -111,18 +111,19 @@
 
     <header class="bg-white shadow">
         <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-            <img src="/img/pria.png" alt="" class="absolute inset-0 -z-10 size-full object-cover object-right md:object-center">
-            <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" aria-hidden="true">
+            <img src="/img/pria.png" alt="" class="absolute inset-0 -z-10 size-full object-cover object-right md:object-center bg-black opacity-30 -z-1 ">
+            <div class=""></div>
+            <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl -z-2" aria-hidden="true">
               <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
             </div>
             <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu" aria-hidden="true">
               <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
             </div>
-            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8 z-20">
               <div class="mx-auto max-w-2xl lg:mx-0">
                 <p class="mt-8 text-pretty text-lg font-medium text-gray-300 sm:text-xl/8">Posted on <b>startup</b></p>
                 <h2 class="text-5xl font-semibold tracking-tight text-white sm:text-7xl">Step-by-step guide to choosing great font pairs</h2>
-                <p class="mt-8 text-pretty text-lg font-medium text-gray-300 sm:text-xl/8">By<span class="text-yellow-600 font-semibold">James West </span>|  May 23, 2025 </p>
+                <p class="mt-8 text-pretty text-lg font-medium text-gray-300 sm:text-xl/8">By<span class="text-yellow-600 font-semibold"> James West </span>|  May 23, 2025 </p>
                 <p class="mt-8 text-pretty text-lg font-medium text-gray-300 sm:text-xl/8 pb-5">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.</p>
                 <button class="bg-yellow-500 text-white px-6 py-3 font-semibold hover:bg-yellow-600 rounded">
                     Raed More >
@@ -134,11 +135,8 @@
     </header>
 
     <main class="flex-grow pt-20">
-        <!-- Container Utama -->
     <div class="max-w-6xl mx-auto px-4 py-10">
-    <!-- Bagian Atas: Featured Post & All Posts -->
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12">
-        <!-- Featured Post -->
         <div class="md:col-span-2 bg-white shadow-lg rounded-lg">
             <img src="/img/bangunan.png" alt="Featured Image" class="rounded-t-lg w-full h-64 object-cover">
             <div class="p-6">
